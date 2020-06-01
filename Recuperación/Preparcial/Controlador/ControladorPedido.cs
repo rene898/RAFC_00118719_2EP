a81a2a6d429dfd35743c1f3edf0c2417ffc8c5fc
rene898 <54415136+rene898@users.noreply.github.com>
2020-05-31 18:56:10 -0600

    Add: Recuperacion

---
+++ Recuperación/Preparcial/Controlador/ControladorPedido.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Preparcial.Controlador
+{
+    public static class ControladorPedido
+    {
+        public static DataTable GetPedidosUsuarioTable(string id)
+        {
+            DataTable pedidos = null;
+
+            try
+            {   //cambio de nombreArticulo a nombreArt y arreglar consultas de GetPedidosUsuarioTable
+                pedidos = ConexionBD.EjecutarConsulta("SELECT p.idPedido, i.nombreArt, p.cantidad, i.precio" +
+                                            " FROM PEDIDO p, INVENTARIO i, USUARIO u" +
+                                            " WHERE p.idArticulo = i.idArticulo" +
+                                            " AND p.idUsuario = u.idUsuario" +
+                                            $" AND u.idUsuario = {id}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+
+            return pedidos;
+        }
+
+        public static DataTable GetPedidosTable()
+        {
+            DataTable pedidos = null;
+
+            try
+            {   //cambio de nombreArticulo a nombreArt y arreglar consultas de GetPedidosTable
+                pedidos = ConexionBD.EjecutarConsulta("SELECT p.idPedido, i.nombreArt, p.cantidad, i.precio" +
+                                            " FROM PEDIDO p, INVENTARIO i, USUARIO u" +
+                                            " WHERE p.idArticulo = i.idArticulo" +
+                                            " AND p.idUsuario = u.idUsuario");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+
+            return pedidos;
+        }
+
+        public static void HacerPedido(string idUsuario, string idArticulo, string cantidad)
+        {
+            try
+            {
+                ConexionBD.EjecutarComando("INSERT INTO PEDIDO(idUsuario, idArticulo, cantidad) " +
+                    $"VALUES({idUsuario}, {idArticulo}, {cantidad})");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+        }
+    }
+}
---
+++ Recuperación/Preparcial/Modelo/Usuario.cs
@@ -0,0 +1,23 @@
+﻿namespace Preparcial.Modelo
+{
+    public class Usuario
+    {
+        public string IdUsuario { get; set; }
+
+        public string NombreUsuario { get; set; }
+
+        public string Contrasena { get; set; }
+
+        public bool Admin { get; set; }
+
+        
+        public Usuario(string idUsuario, string nombre, string contrasenia, bool admin)
+        {
+            IdUsuario = idUsuario;
+            //cambio de nombreUsuario a nombre
+            NombreUsuario = nombre;
+            Contrasena = contrasenia;
+            Admin = admin;
+        }
+    }
+}
